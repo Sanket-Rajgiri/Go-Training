49a62a9bc2001095130892c7084648b3f1fe576f
Sanket-Rajgiri <sanketrajgiri279@gmail.com>
2025-04-23 23:32:31 -0700

    Updated code for story 5

--- wc/main.go
+++ wc/main.go
@@ -2,130 +2,137 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"flag"
 	"fmt"
 	"os"
-	"slices"
 )
+
+type errorCode int
 
 const (
-	lineFlag      = "-l"
-	wordFlag      = "-w"
-	characterFlag = "-c"
-	helpFlag      = "-h"
+	fileNotExistErrorCode         errorCode = 1
+	filePermissionDeniedErrorCode errorCode = 2
+	IsDirectoryErrorCode          errorCode = 21
+	unexpectedErrorCode           errorCode = 125
+	programName                             = "wc"
 )
 
-func lineCount(filepath string) (int, error) {
-	lineCounter := 0
-	f, _ := os.Open(filepath)
-	defer f.Close()
+var (
+	ErrIsDirectory      = errors.New("is a directory")
+	ErrPermissionDenied = errors.New("permission denied")
+	ErrFileNotExist     = errors.New("no such file or directory")
+	ErrMissingArguments = errors.New("missing arugments")
+)
 
-	// reader := bufio.NewReader(f)
-	// for {
-	// 	_, err := reader.ReadString(lineDelimiter)
-	// 	if err != nil {
-	// 		if err == io.EOF {
-	// 			lineCounter += 1
-	// 			break
-	// 		} else {
-	//
-	// 			return 0, err
-	// 		}
-	// 	}
-	// 	lineCounter += 1
-	// }
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		lineCounter++
-	}
-	if err := scanner.Err(); err != nil {
-		return 0, err
-	}
-
-	return lineCounter, nil
+type flagState struct {
+	countLines bool
+	countWords bool
+	countBytes bool
 }
 
-func byteCount(filepath string) (int, error) {
-	byteCounter := 0
-	f, _ := os.Open(filepath)
+func countWithSplit(filepath string, split bufio.SplitFunc) (int, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return 0, err
+	}
 	defer f.Close()
-
 	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanBytes)
+	scanner.Split(split)
+	count := 0
 	for scanner.Scan() {
-		byteCounter++
+		count++
 	}
 	if err := scanner.Err(); err != nil {
 		return 0, err
 	}
-	return byteCounter, nil
+	return count, nil
+}
+
+func lineCount(filepath string) (int, error) {
+	return countWithSplit(filepath, bufio.ScanLines)
 }
 
 func wordCount(filepath string) (int, error) {
-	wordCounter := 0
-	f, _ := os.Open(filepath)
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanWords)
-	for scanner.Scan() {
-		wordCounter++
-	}
-	if err := scanner.Err(); err != nil {
-		return 0, err
-	}
-	return wordCounter, nil
+	return countWithSplit(filepath, bufio.ScanWords)
 }
-func output(args []string) string {
-	var output string
-	filepath := args[len(args)-1]
-	if slices.Contains(args, lineFlag) {
+
+func byteCount(filepath string) (int, error) {
+	return countWithSplit(filepath, bufio.ScanBytes)
+}
+
+func cliOutput(wcflagState flagState, filepath string) (string, errorCode, error) {
+	var outputString string
+	if wcflagState.countLines {
 		lines, err := lineCount(filepath)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s %s\n", args[0], err)
-			os.Exit(125)
+			return " ", unexpectedErrorCode, err
 		}
-		output += fmt.Sprintf("%8d", lines)
+		outputString += fmt.Sprintf("%1d", lines)
 	}
-	if slices.Contains(args, wordFlag) {
-		words, err := wordCount(filepath)
+	if wcflagState.countWords {
+		lines, err := wordCount(filepath)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s %s\n", args[0], err)
-			os.Exit(125)
+			return " ", unexpectedErrorCode, err
 		}
-		output += fmt.Sprintf("%8d", words)
+		outputString += fmt.Sprintf("%1d", lines)
 	}
-
-	if slices.Contains(args, characterFlag) {
-		bytes, err := byteCount(filepath)
+	if wcflagState.countBytes {
+		lines, err := byteCount(filepath)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s %s\n", args[0], err)
-			os.Exit(125)
-		}
-		output += fmt.Sprintf("%8d", bytes)
-	}
+			return " ", unexpectedErrorCode, err
 
-	return output + " " + filepath
+		}
+		outputString += fmt.Sprintf("%1d", lines)
+	}
+	return outputString, 0, nil
+}
+
+func flagParser() flagState {
+	lineFlag := flag.Bool("l", false, "count lines")
+	wordFlag := flag.Bool("w", false, "count words")
+	byteFlag := flag.Bool("c", false, "count bytes")
+	flag.Parse()
+	if !*lineFlag && !*wordFlag && !*byteFlag {
+		return flagState{countLines: true, countWords: true, countBytes: true}
+	}
+	return flagState{countLines: *lineFlag, countWords: *wordFlag, countBytes: *byteFlag}
+}
+
+func checkFile(filepath string) (errorCode errorCode, err error) {
+	fileinfo, err := os.Stat(filepath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fileNotExistErrorCode, ErrFileNotExist
+		} else if os.IsPermission(err) {
+			return filePermissionDeniedErrorCode, ErrPermissionDenied
+		} else {
+			return unexpectedErrorCode, err
+		}
+	}
+	if fileinfo.IsDir() {
+		return IsDirectoryErrorCode, ErrIsDirectory
+	}
+	return 0, nil
 }
 
 func main() {
 	args := os.Args
-	filepath := args[len(args)-1]
-	fileinfo, err := os.Stat(filepath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s %s\n", args[0], err)
-		fmt.Println(fileinfo.IsDir())
-		if os.IsNotExist(err) {
-			os.Exit(2)
-		} else if os.IsPermission(err) {
-			os.Exit(1)
-		} else {
-			os.Exit(125)
-		}
-	} else if fileinfo.IsDir() {
-		fmt.Fprintf(os.Stderr, "%s read %s: Is a directory\n", args[0], filepath)
-		os.Exit(21)
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", programName, ErrMissingArguments)
+		os.Exit(int(unexpectedErrorCode))
 	}
-	fmt.Fprintf(os.Stdout, "%s\n", output(args))
+	flagState := flagParser()
+	filepath := args[len(args)-1]
+	exitCode, err := checkFile(filepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s: %3s\n", programName, filepath, err)
+		os.Exit(int(exitCode))
+	}
+	output, exitCode, err := cliOutput(flagState, filepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s: %3s\n", programName, filepath, err)
+		os.Exit(int(exitCode))
+	}
+	fmt.Printf("%8s %s\n", output, filepath)
 }
--- wc/main_test.go
+++ wc/main_test.go
@@ -5,21 +5,54 @@ import (
 )
 
 type test struct {
-	name     string
-	filePath string
-	want     int
+	name              string
+	filePath          string
+	want              int
+	expectedError     error
+	expectedErrorCode int
 }
 
+func Test_checkFile(t *testing.T) {
+	tests := []test{
+		{
+			name:              "test1",
+			filePath:          "test/test1.txt",
+			expectedError:     nil,
+			expectedErrorCode: 0,
+		},
+		{
+			name:              "test2",
+			filePath:          "test/test3.txt",
+			expectedError:     ErrFileNotExist,
+			expectedErrorCode: 1,
+		},
+		{
+			name:              "test3",
+			filePath:          "test",
+			expectedError:     ErrIsDirectory,
+			expectedErrorCode: 21,
+		},
+	}
+	for _, test := range tests {
+		errorCode, fileError := checkFile(test.filePath)
+		if test.expectedError != fileError {
+			t.Errorf("Expected error %v but got %v", test.expectedError, fileError)
+		}
+		if test.expectedErrorCode != errorCode {
+			t.Errorf("Expected errorCode %v  but got %v", test.expectedErrorCode, errorCode)
+		}
+	}
+}
 func Test_lineCount(t *testing.T) {
 	tests := []test{
 		{
 			name:     "Test1",
-			filePath: "test1.txt",
+			filePath: "test/test1.txt",
 			want:     10,
 		},
 		{
 			name:     "Test1",
-			filePath: "test2.txt",
+			filePath: "test/test2.txt",
 			want:     0,
 		},
 	}
@@ -39,12 +72,12 @@ func Test_byteCount(t *testing.T) {
 	tests := []test{
 		{
 			name:     "Test1",
-			filePath: "test1.txt",
+			filePath: "test/test1.txt",
 			want:     445,
 		},
 		{
 			name:     "Test1",
-			filePath: "test2.txt",
+			filePath: "test/test2.txt",
 			want:     0,
 		},
 	}
@@ -63,12 +96,12 @@ func Test_wordCount(t *testing.T) {
 	tests := []test{
 		{
 			name:     "Test1",
-			filePath: "test1.txt",
+			filePath: "test/test1.txt",
 			want:     78,
 		},
 		{
 			name:     "Test1",
-			filePath: "test2.txt",
+			filePath: "test/test2.txt",
 			want:     0,
 		},
 	}
